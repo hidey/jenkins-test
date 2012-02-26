6ff30998f059ca354dd9086e079cd8af924e6b87
hideyuki.kikuma <hideyuki.kikuma@gmail.com>
2012-02-27 08:12:30 +0900

    init

---
+++ jenkins/src/test/java/jp/example/jenkins_test/jenkins/JenkinsSampleTest.java
@@ -0,0 +1,30 @@
+package jp.example.jenkins_test.jenkins;
+
+import static org.junit.Assert.*;
+import jp.example.jenkins_test.jenkins.JenkinsSample;
+
+import org.junit.Test;
+
+public class JenkinsSampleTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSample() {
+		JenkinsSample sample = new JenkinsSample();
+		String string = "AAA";
+		sample.setSample(string);
+		assertEquals("getSample",string, sample.getSample());
+	}
+
+	@Test
+	public void testGetSample2() {
+		JenkinsSample sample = new JenkinsSample();
+		String string = "AAA";
+		sample.setSample2(string);
+		assertEquals("getSample2",string, sample.getSample2());
+	}
+}
